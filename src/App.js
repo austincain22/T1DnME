3b5c0949bd8790dc08d9000c410496ec4c37c092
Marissa Nutter <mnutter5@gmail.com>
2019-02-23 14:45:47 -0800

    home page begin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import './Styles/App.css';
+//import './Styles/App.css';
+import Home from './Components/Pages/HomePage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            T1DnMe Home page
-          </p>
-        </header>
+      <div>
+
+            <Home />
+        
       </div>
     );
   }
--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,5 +1,22 @@
+<<<<<<< Updated upstream
 
 ReactDOM.render(
     <h1>Hello, world! This is the home page</h1>,
     document.getElementById('root')
   );
+=======
+import React, { Component } from 'react';
+import '../../Styles/GlobalStyles.css';
+
+class Home extends Component{
+
+    render() {
+        return (
+            <div className="body">This is my home page</div>
+        )
+    }
+
+}
+
+export default Home;
+>>>>>>> Stashed changes
