2579d341c14fc5e92837899630b04b2d14cd1ee3
PumpkinKing93 <tgillette@mail.fresnostate.edu>
2019-02-23 15:39:28 -0800

    moved some files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import Home from './Components/Pages/HomePage';
 
+const routing = (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} /> */}
+      </div>
+    </Router>
+  )
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(
+    routing, document.getElementById('root')
+);
 
