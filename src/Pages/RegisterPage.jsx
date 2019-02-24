f8ee9f7b3623ea0bf90ee5e2590b1c3477a77d99
Austin Cain <austincain22@gmail.com>
2019-02-23 19:54:11 -0800

    Registers users

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -21,6 +21,9 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary
+  },
+  input: {
+    padding: '2%'
   }
 })
 
@@ -30,21 +33,45 @@ class Register extends React.Component {
     super(props)
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      firstN: "",
+      lastN: "",
     }
-    this.signUp = this.signUp.bind(this)
+    this.signUp = this.signUp.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.initializeDoc = this.initializeDoc.bind(this);
+  }
+
+  async initializeDoc() {
+    const newUse = await db.collection('Users').doc(`${auth.currentUser.uid}`);
+    await newUse.get().then(async () => {
+      await newUse.set({      // create the document if it's a new user
+        childrenID: [],
+        firstName: this.state.firstN,
+        lastName: this.state.lastN,
+      });
+    }).catch((error) => {   //if an error occurs, alert user of the error
+      window.alert(error);
+    });
   }
 
   async signUp(e) {
     e.preventDefault()
-    auth
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then({
-        // User signed in
-      })
-      .catch(error => {
-        window.alert(error)
-      })
+    if (this.state.firstN === "") {
+      window.alert("Fill in your first name.");
+    }
+    else if (this.state.lastN === "") {
+      window.alert("Fill in you last name.");
+    }
+    else {
+      await auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(async () => {
+          await this.initializeDoc();
+        })
+        .catch(error => {
+          window.alert(error)
+        })
+    }
   }
 
   async googleSignUp() {
@@ -56,20 +83,30 @@ class Register extends React.Component {
       })
   }
 
+  //handle changes from email and password
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <div className={classes.root}>
         <Grid container spacing={24} justify="center">
-          <Grid item xs={3} >
+          <Grid item xs={3} zeroMinWidth>
             <Paper className={classes.paper}>
-              User Registration
-              <form>
-                <Input fullWidth={true} placeholder="Email..." />
-                <Input fullWidth={true} placeholder="Password..." />
+              <h2>User Registration</h2>
+              <form className={classes.input}>
+                <Input onChange={this.handleChange} name="firstN" value={this.state.firstN} className={classes.input} fullWidth={true} placeholder="First Name" />
+                <Input onChange={this.handleChange} name="lastN" value={this.state.lastN} className={classes.input} fullWidth={true} placeholder="Last Name" />
+                <Input onChange={this.handleChange} name="email" value={this.state.email} className={classes.input} fullWidth={true} placeholder="Email..." />
+                <Input onChange={this.handleChange} name="password" value={this.state.password} className={classes.input} fullWidth={true} placeholder="Password..." />
               </form>
-              <Button color="primary" fullWidth={true} size="large">Register</Button>
+              <Button onClick={this.signUp} className={classes.input} color="primary" fullWidth={true} size="large">Register</Button>
             </Paper>
           </Grid>
         </Grid>
