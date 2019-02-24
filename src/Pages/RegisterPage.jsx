cfb78729c749fd2fa066bfbfe27beef78e6aee43
Austin Cain <austincain22@gmail.com>
2019-02-23 18:48:09 -0800

    Register form kind of

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 
 import '../Styles/User.css'
 import { auth } from '../Config/fire'
-// import { db } from '../Config/fire';
+import { db } from '../Config/fire';
 import { provider } from '../Config/fire'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import fire from '../Config/fire'
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
-class Register extends Component {
-  constructor (props) {
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+})
+
+
+class Register extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
       email: '',
@@ -15,7 +35,7 @@ class Register extends Component {
     this.signUp = this.signUp.bind(this)
   }
 
-  async signUp (e) {
+  async signUp(e) {
     e.preventDefault()
     auth
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
@@ -27,7 +47,7 @@ class Register extends Component {
       })
   }
 
-  async googleSignUp () {
+  async googleSignUp() {
     auth
       .signInWithPopup(provider)
       .then({})
@@ -36,19 +56,39 @@ class Register extends Component {
       })
   }
 
-  render () {
+  render() {
+    const { classes } = this.props;
+
     return (
-      <div className='App'>
-        <header className='App-header'>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <input type='text' name='email' placeholder='Email...' />
-          <input type='text' name='password' placeholder='Password...' />
-          <button onClick={this.signUp}>Register</button>
-          <button onClick={this.googleSignUp}>SignUp with Google</button>
-        </header>
+      <div className={classes.root}>
+        <Grid container spacing={24} justify="center">
+          <Grid item xs={3} >
+            <Paper className={classes.paper}>
+              User Registration
+              <form>
+                <Input fullWidth={true} placeholder="Email..." />
+                <Input fullWidth={true} placeholder="Password..." />
+              </form>
+              <Button color="primary" fullWidth={true} size="large">Register</Button>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
+      // <div className='App'>
+      //   <header className='App-header'>
+      //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      //     <input type='text' name='email' placeholder='Email...' />
+      //     <input type='text' name='password' placeholder='Password...' />
+      //     <button onClick={this.signUp}>Register</button>
+      //     <button onClick={this.googleSignUp}>SignUp with Google</button>
+      //   </header>
+      // </div>
     )
   }
 }
 
-export default Register
+Register.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Register)
