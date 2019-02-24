a4772165bbff8c494e5cd263291ef8fbc7753d59
Austin Cain <austincain22@gmail.com>
2019-02-24 02:43:20 -0800

    comment

--- src/Config/fire.jsx
+++ src/Config/fire.jsx
@@ -1,18 +1,29 @@
 import firebase from "firebase";
 
 const config = {
-     apiKey: "AIzaSyAuY3eV2AgbmCGYFDd9KViYc1F44KE3gyU",
-     authDomain: "t1dnme-a1012.firebaseapp.com",
-     databaseURL: "https://t1dnme-a1012.firebaseio.com",
-     projectId: "t1dnme-a1012",
-     storageBucket: "t1dnme-a1012.appspot.com",
-     messagingSenderId: "907329423038"
- };
+    apiKey: "AIzaSyAuY3eV2AgbmCGYFDd9KViYc1F44KE3gyU",
+    authDomain: "t1dnme-a1012.firebaseapp.com",
+    databaseURL: "https://t1dnme-a1012.firebaseio.com",
+    projectId: "t1dnme-a1012",
+    storageBucket: "t1dnme-a1012.appspot.com",
+    messagingSenderId: "907329423038"
+};
 
- const fire = firebase.initializeApp(config); // create a variable for short hand ReferenceError.
+const fire = firebase.initializeApp(config); // create a variable for short hand ReferenceError.
 
- export default fire; // allow other files in the project to be able to access the information.
- export const provider = new firebase.auth.GoogleAuthProvider();
- export const auth = fire.auth();
- export const db = fire.firestore();
+export default fire; // allow other files in the project to be able to access the information.
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = fire.auth();
+export const db = fire.firestore();
+
+
+//  auth.onAuthStateChanged((user) =>{
+//     if(user){
+//         //user logged in
+//     }
+//     else{
+//         //no user 
+//     }
+//  })
+
 
